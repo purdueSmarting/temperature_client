c58a26e72e3c2cd6dc5c2f1ed08965d485017611
SuYunPark <spark5@nate.com\]>
2017-11-09 13:44:24 -0500

    temperature simulation

--- temp.py
+++ temp.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# coding=utf8
 import Adafruit_DHT as dht
 import datetime
 from threading import Timer
@@ -7,33 +9,54 @@ import json
 import urllib2
 import httplib as http
 import time
+import serial
 
-def printit():
-        wtime = datetime.datetime.now()
-        humidity, temperature = dht.read_retry(dht.DHT22, 4)
-	return wtime, humidity, temperature
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=2)
+ser.open
+global previous_temperature
 
-while 1:
-        wtime, humidity, temperature = printit()
-        
-        response = requests.get('http://13.59.174.162:7579/temperature')
-        req_temp = response.text
-        print req_temp
-
-        diff_temp = float(req_temp) - temperature
-        state = ""
-        if diff_temp > 0 :
-                state = "up"
-        elif diff_temp < 0 :
-                state = "down"
+def temp_print(previous_temperature, current_temperature):
+    #제대로 센서가 값을 읽어왔을 때
+    if current_temperature > 20 and current_temperature < 30:
+        if previous_temperature is current_temperature :
+            print "Previous Temperature: " + str(previous_temperature) + " Current Temperature: " + str(current_temperature)
+            print "Not Changed"
         else :
-                state = "..."
+            temperature = "Previous Temperature: " + str(previous_temperature) + " Current Temperature: " + str(current_temperature)
+            print temperature
+            
+def temp_on(current_temperature, previous_temperature, req_temp):
+    #에러 체크 - previous 온도와 current 온도가 10도 이상 차이나면 오류 발생
+    if abs(current_temperature - previous_temperature) > 10:
+        print False
+    #request 온도가 20도 이하가 되면 히터 켜줌
+    elif req_temp <= 20 :
+        print "Turn heat on"
+    #request 온도가 30도 이상이 되면 에어컨 켜줌
+    elif req_temp >= 30 :
+        print "Turn cool on"
+    else :
+        print "Do not touch"
+             
+while 1:
+    previous_temperature = int(ser.readline())
+    current_temperature = int(ser.readline())
+    current_time = datetime.datetime.now()
+    #에어컨의 희망 온도 받아오기
+    response = requests.get('http://192.168.2.136:7579/temperature')
+    req_temp = int(response.text)
+    print req_temp
+    
+    temp_print(previous_temperature, current_temperature)
+    temp_on(current_temperature, previous_temperature, req_temp)
 
-        data = "Time:" + str(wtime) + ' Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature,humidity)
-        data = json.loads(json.dumps(data))
-        print data
-        print "{0:0.1f}".format(abs(diff_temp)) + " " + state
-        
-	r = requests.post('http://13.59.174.162:7579/temperature', json={'data': data})
-	print r.text
-	time.sleep(10)
+    print "\n"
+    data = "Time:" + str(current_time) + str(temp_print(previous_temperature, current_temperature)) + str(temp_on(current_temperature, previous_temperature, req_temp))
+    data = json.loads(json.dumps(data))
+    print data
+    r = requests.post('http://192.168.2.136:7579/temperature', json={'data': data})
+    print r.text
+
+    previous_temperature = current_temperature
+    time.sleep(2)
+    
