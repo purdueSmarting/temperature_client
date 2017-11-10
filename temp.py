55e190f3cf6b57bde9f817f4c71c895315fb07bc
SuYunPark <spark5@nate.com\]>
2017-11-09 19:11:38 -0500

    to json

--- temp.py
+++ temp.py
@@ -13,6 +13,7 @@ import serial
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=2)
 ser.open
+
 global previous_temperature
 
 def temp_print(previous_temperature, current_temperature):
@@ -37,13 +38,15 @@ def temp_on(current_temperature, previous_temperature, req_temp):
         print "Turn cool on"
     else :
         print "Do not touch"
-                
-while 1:
-    previous_temperature = int(ser.readline())    
-    current_temperature = int(ser.readline())
-    current_time = datetime.datetime.now()
+
+previous_temperature = int(ser.readline())
+
+while True:
+    print "loop start"
+    current_temperature = int(ser.readline(), base=10)
+    current_time = str(datetime.datetime.now())
     #에어컨의 희망 온도 받아오기
-    response = requests.get('http://192.168.2.136:7579/temperature')
+    response = requests.get('http://13.59.174.162:7579/temperature')
     req_temp = int(response.text)
     print req_temp
     
@@ -51,12 +54,11 @@ while 1:
     temp_on(current_temperature, previous_temperature, req_temp)
 
     print "\n"
-    data = "Time:" + str(current_time) + str(temp_print(previous_temperature, current_temperature)) + str(temp_on(current_temperature, previous_temperature, req_temp))
-    data = json.loads(json.dumps(data))
-    print data
-    r = requests.post('http://192.168.2.136:7579/temperature', json={'data': data})
-    print r.text
-
+    payload = {'time':current_time,'current_temperature':current_temperature}
+    jsonString = json.dumps(payload)
+    print (jsonString)
+    requests.post('http://13.59.174.162:7579/temperature', data=payload)
+    print "request posted"
     previous_temperature = current_temperature
     time.sleep(2.5)
-    
+    print "loop finished"
